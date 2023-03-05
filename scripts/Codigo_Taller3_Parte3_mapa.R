f0de15f57f706a1f85f5e3a1c3addbd2d021eabe
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-03-05 15:09:52 -0500

    codigo 3
    codigo 3 mapa

---
+++ scripts/Codigo_Taller3_Parte3_mapa.R
@@ -0,0 +1,64 @@
+#_____________________________________________________________________________#
+#                                                                             #
+#                      Problem Set 3: Making Money with ML?                     #
+# 
+#
+# Código predictores externos
+#_____________________________________________________________________________#
+
+#   Autores: - Jorge Rodríguez                                                  
+#            - Iván Velázquez  
+#            - Santiago Gonzalez
+#            - Maria Jose Colmenares
+#
+#  Fecha: 12/03/2023 
+
+
+# - Librerias y paquetes 
+
+library(pacman)
+p_load(tidyverse, rstudioapi, rio, leaflet, rgeos, tmaptools, sf, stargazer,osmdata, plotly)
+
+
+
+# 2.1 Train
+
+grafica_train_completa <-  leaflet() %>% addTiles() %>%
+  addPolygons(data = parques_geometria, col ="yellow",
+              opacity = 0.8, popup = parques_geometria$name) %>%
+  addCircles(lng = train$lon, 
+             lat = train$lat,
+             fillOpacity = 1,
+             col = color,
+             radius = r_scaled_test,
+             popup = html_popup_test,
+             opacity = 1)%>%
+  addPolygons(data=hospital_geometria , col="#f2542d")%>% #hospitales
+  addPolygons(data=clinic_geometria , col="#f2542d")%>% #hospitales
+  addPolygons(data=bus_station_geometria , col="#0e9594")%>%#buses
+  addPolygons(data=police_geometria , col="#f5dfbb")%>%#policia
+  addPolygons(data=school_geometria , col="#0d3b66")%>%#colegio
+  addPolygons(data=restaurant_geometria , col="#55286f")#restaruante
+htmlwidgets::saveWidget(grafica_train_completa, "views//grafica_train_completa.html") 
+
+
+
+grafica_test_completa <-  leaflet() %>% addTiles() %>%
+  addPolygons(data = parques_geometria, col ="yellow",
+              opacity = 0.8, popup = parques_geometria$name) %>%
+  addCircles(lng = test$lon, 
+             lat = test$lat,
+             fillOpacity = 1,
+             col = color,
+             radius = r_scaled_test,
+             popup = html_popup_test,
+             opacity = 1)%>%
+  addPolygons(data=hospital_geometria , col="#f2542d")%>% #hospitales
+  addPolygons(data=clinic_geometria , col="#f2542d")%>% #hospitales
+  addPolygons(data=bus_station_geometria , col="#0e9594")%>%#buses
+  addPolygons(data=police_geometria , col="#f5dfbb")%>%#policia
+  addPolygons(data=school_geometria , col="#0d3b66")%>%#colegio
+  addPolygons(data=restaurant_geometria , col="#55286f")#restaruante
+htmlwidgets::saveWidget(grafica_test_completa, "views//grafica_test_completa.html") 
+
+

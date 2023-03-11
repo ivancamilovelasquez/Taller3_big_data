4d0736af2b6fb25e483d881f960e6d556c5669e5
mjcolmenares <124100468+mjcolmenares@users.noreply.github.com>
2023-03-10 21:43:51 -0500

    Update Taller 3 codigo.R

--- scripts/Taller 3 codigo.R
+++ scripts/Taller 3 codigo.R
@@ -23,6 +23,30 @@ p_load(tidyverse, rstudioapi, rio, leaflet, rgeos, tmaptools, sf, stargazer,osmd
 test_final <- read.csv("~/MAESTRIA/Taller 3 Big Data/test_final.csv")
 train_final <- read.csv("~/MAESTRIA/Taller 3 Big Data/train_final.csv")
 
+#Cambiar nombre de columnas ----
+colnames(test_final)[28] <- "dist_min_train_parque"
+colnames(test_final)[29] <- "dist_min_train_fitness"
+colnames(test_final)[30] <- "dist_min_train_fitnesse"
+colnames(test_final)[31] <- "dist_min_train_playground"
+colnames(test_final)[33] <- "dist_min_train_sportc"
+colnames(test_final)[34] <- "dist_hospital_centre_train"
+colnames(test_final)[35] <- "dist_clinic_centre_train"
+colnames(test_final)[36] <- "dist_busstation_centre_train"
+colnames(test_final)[37] <- "dist_police_centre_train" 
+colnames(test_final)[38] <- "dist_pub_centre_train"
+colnames(test_final)[39] <- "dist_school_centre_train"
+colnames(test_final)[40] <- "dist_cinema_centre_train"
+colnames(test_final)[41] <- "dist_restaurante_centre_train"
+
+train_final$garaje <- as.factor(train_final$garaje)
+train_final$piscina <-  as.factor(train_final$piscina)
+train_final$terraza <- as.factor(train_final$terraza)
+train_final$campestre <- as.factor(train_final$campestre) 
+
+test_final$garaje <- as.factor(test_final$garaje)
+test_final$piscina <-  as.factor(test_final$piscina)
+test_final$terraza <- as.factor(test_final$terraza)
+test_final$campestre <- as.factor(test_final$campestre) 
 
 #Partir la base de Train en 2: t_train y t_test
 p_load(caret)
